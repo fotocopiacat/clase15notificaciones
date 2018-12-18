90f81cdbb29b852300a8adef69a21a404a603408
fotocopia <catalina.pimentel@uniacc.edu>
2018-12-18 19:24:20 -0300

    1st commit

---
+++ app/src/main/java/com/example/android/clase15notificaciones/MainActivity.kt
@@ -0,0 +1,60 @@
+package com.example.android.clase15notificaciones
+
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.content.Context
+import android.content.Intent
+import android.graphics.Color
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.support.v4.app.NotificationCompat
+import android.support.v4.app.TaskStackBuilder
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+        //SE CREA EL CANAL DE LA NOTIFICACION
+    btnNotificacion.setOnClickListener {
+        val id = "mi_canal_01"
+        val nombre = getString(R.string.abc_action_bar_home_description)
+        val description = getString(R.string.abc_action_bar_home_description)
+        val importancia = NotificationManager.IMPORTANCE_HIGH
+        val canal = NotificationChannel(id, nombre, importancia)
+
+        canal.description = description
+        canal.enableLights(true)
+        canal.lightColor = Color.RED
+        canal.enableVibration(true)
+
+        //builder de la notificacion
+        val builder = NotificationCompat.Builder(this, id)
+        builder.setSmallIcon(R.drawable.notification_icon_background)
+        builder.setContentTitle("mi notificacion")
+        builder.setContentText("mensaje de notificacion")
+
+        val intento = Intent(this,MainActivity::class.java)
+        val stackBuilder = TaskStackBuilder.create(this)
+        stackBuilder.addNextIntent(intento)
+
+        val resultado = stackBuilder.getPendingIntent(0, PendingIntent.FLAG_UPDATE_CURRENT)
+        builder.setContentIntent(resultado)
+
+        val notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        notificationManager.createNotificationChannel(canal)
+        notificationManager.notify(0,builder.build())
+
+    }
+
+
+
+    }
+
+
+    }
+
